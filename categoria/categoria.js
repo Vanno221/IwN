d2e944c0479d1a377814058cbc974d5f0ac4159a
Tommaso <tommaso.vanno@gmail,com>
2017-06-19 18:03:56 +0200

    Aggiunta lista Rss Utente

--- categoria/categoria.js
+++ categoria/categoria.js
@@ -1,10 +1,49 @@
-angular.module('IwN').controller('CategoriaCtrl',function($scope,$firebaseArray){
+angular.module('IwN').controller('CategoriaCtrl',function($scope,$firebaseArray,$firebaseAuth,$state){
    
-   var ref = firebase.database().ref();
+   var categorie = []; // puoi scrivere anche new Array()
+   var ref = firebase.database().ref("UrlRss");
    var list = $firebaseArray(ref);
    $scope.list = list;
-  /*    $scope.setCategory = function() {
-        console.log("Set category to: "+$scope.selectedCategory)
-		$location.path('/home/'+$scope.selectedCategory);
-    } */
+
+    /*globals alert */
+    var auth = $firebaseAuth(); // importa firebaseauth
+    
+
+    // Funzione per aggiungere o rimuovere id selezionato
+    $scope.selectCategoria = function(id) {
+        var index = categorie.indexOf(id); // restituisce l'indice del id che gli ho passato
+        if(index > -1){ // se l'indice è maggiore di -1, quindi esiste
+            categorie.splice(index, 1); // rimuovi id dal array
+            return;
+        }
+        categorie.push(id); // altrimenti aggiungi id ad array
+    };
+
+    // Funzione di visualizzazione se id è stato selezionato o no
+    $scope.hasSelect = function(id) {
+        var index = categorie.indexOf(id);
+        if(index > -1) {
+            return 'list-group-item-success';
+        }
+        return 'not-selected';
+    };
+
+    $scope.saveCategorie = function() {
+        var ref = firebase.database().ref('RssUtente');
+        var rssUsers = $firebaseArray(ref);
+        var authData = auth.$getAuth(); 
+
+        var rssUser = {
+            IdUtente : authData.uid, // qui dobbiamo inserire l'id del utente loggato
+            RssSelected: categorie
+        }
+
+        rssUsers.$add(rssUser)
+            .then(function (ref) {
+                $state.go('news'); // naviga in news
+            }, function (error) {
+                console.error("Error:", error);
+                alert('Errore: Effettuare il login');
+            });
+    };
 });
--- news/news.js
+++ news/news.js
@@ -1,15 +1,69 @@
-angular.module('IwN').controller('NewsCtrl',function($scope,$stateParams,newsFactory){
+angular.module('IwN').controller('NewsCtrl', function ($scope, newsFactory, $firebaseArray, $firebaseAuth, $state) {
 
-    	console.log($stateParams.category);
+	$scope.rssUtente = {};
+	$scope.rssUrls  = [];
 
-		$scope.category=$stateParams.category;
+	firebase.database(); //ma qui non ci va  ;?
 
-		newsFactory.getNews($scope.category)
+    /*globals alert */
+    var auth = $firebaseAuth(); // importa firebaseauth
 
-        
-		.success( function (data, status, headers, config){
-				console.log("SUCCESS",data.query.results.item);
-				$scope.news_list=data.query.results.item;
-			});
+	$scope.init = function () {
+				
+		var authData = auth.$getAuth();
+		var IdAuth; //Id utente
 
+		if (authData) {
+			IdAuth = authData.uid;  // e qui?
+		} else {
+			console.log("Signed out");
+		}
+
+
+		var ref = firebase.database().ref("RssUtente"); // Invece è corretto
+		var list = $firebaseArray(ref);
+		
+		var refUrls = firebase.database().ref("UrlRss");
+		var listUrls = $firebaseArray(refUrls);
+		$scope.rssUrls = listUrls;
+
+
+		// angular.forEach(list, function(value, key2) { // molto probabilmente non funziona questo, provalo comunque
+		// 	if (idUtente === value.IdAuth) {
+		// 		// non se sa va bene 
+		// 	}
+   		// });
+
+		list.$loaded(function (res) { // carica tutte le configurazioni
+			for (var i = 0; i < res.length; i++) { // ciclo le configurazioni
+				console.debug(res[i]);
+
+				if(res[i].idUtente == IdAuth) {
+					$scope.rssUtente = res[i]; // qui hai l'oggetto che si trova sul db di firebase
+					return;
+				}
+			}
+		}, function (err) {
+			console.error(err);
+		});
+	};
+
+	$scope.getRssFeed = function(url) {
+
+		for(var i = 0; i < $scope.rssUrls.length; i++) {
+
+			if($scope.rssUrls[i].Id == url) {
+
+				// restituisce rss
+				return newsFactory
+					.getNews($scope.category)
+					.success(function (data, status, headers, config) {
+						console.log("SUCCESS", data.query.results.item);
+						return data.query.results.item;
+					});
+			}
+		}
+
+		return null;
+	}
 });
